11585d639e62ab3dab06aa24810a3a0aedcbfc1e
beta <beta@hai.haus>
2024-11-16 15:23:21 -0800

    feat: Added method to get Labrinth API version

--- modrinth/__init__.py
+++ modrinth/__init__.py
@@ -6,3 +6,12 @@ from warnings import warn
 
 __version__ = '1.0.0'
 
+LABRINTH_BASE_URL = 'https://api.modrinth.com'
+
+def getLabrinthVersion() -> str:
+    '''
+    getLabrinthVersion gets the current online version of the Labrinth API.
+    '''
+
+    r = requests.get(LABRINTH_BASE_URL + '/')
+    return r.json()['version']
